996cca5ccdacbbd5fff7f52cebbc149bacf5971f
Blond-in-Blue <hholder@me.com>
2017-03-14 16:26:45 -0500

    Copy over UDP code from Chandler's PA1

--- Data Comm Program 2/client.cpp
+++ Data Comm Program 2/client.cpp
@@ -1,15 +1,89 @@
-//
-//  client.cpp
-//  Data Comm Program 2
-//
-//  Created by Hunter Holder and Chandler Dill on 3/10/17.
-//  Copyright © 2017 Hunter Holder. All rights reserved.
-//
-
+using namespace std;
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <assert.h>
+#include <string>
+#include <string.h>
+#include <netinet/in.h>
+#include <stdio.h>
 #include <iostream>
+#include <fstream>
+#include <ostream>
+#include <time.h>
+#include <unistd.h>
+#include <stdlib.h>
+//#######################################################################################################
 
-int main(int argc, const char * argv[]) {
-	// insert code here...
-	std::cout << "Hello, World!\n";
-    return 0;
-}
+//Code written by Chandler Dill, crd248, for the class of Data Communications taught at Mississippi State University during Spring of 2017. This program is the client of a client/server interaction that creates a TCP socket, negotiates a random port, then transfers a datagram over UDP through that negotiated port.
+//Feb 10th, 2017
+
+//#######################################################################################################
+
+
+
+int main(int argc, char ** argv){
+	int mysocket = socket(AF_INET, SOCK_STREAM, 0);	//declares socket
+	int negotPort = atoi(argv[2]);
+	struct hostent *s;			//holds IP address
+	s = gethostbyname(argv[1]);
+	struct sockaddr_in server;
+	memset((char *) &server, 0, sizeof(server));		//setting Destination info
+	server.sin_family = AF_INET;
+	server.sin_port = htons(negotPort);
+	bcopy((char *)s->h_addr, (char *)&server.sin_addr.s_addr, s->h_length);
+	
+	
+	connect(mysocket, (struct sockaddr *)&server, sizeof(server));
+	
+	
+	char * chunksArray;
+	int fileSize;				//open the file, grab the total length
+	FILE * txtFile;
+	txtFile = fopen((argv[3]), "rb");
+	if (txtFile == NULL){
+		cout << "Error, check file." << endl;
+		exit(1);
+	}
+	fseek(txtFile, 0, SEEK_END);
+	fileSize = ftell(txtFile);		//then close the file, to be opened again
+	fclose(txtFile);
+	
+	
+	txtFile = fopen((argv[3]), "rb");
+	if (txtFile == NULL){				//open file for parsing the payload
+		cout << "Error, check file." << endl;
+		exit(1);
+	}
+	
+	
+	chunksArray = (char*) malloc (sizeof(char)*fileSize);
+	size_t results = fread(chunksArray, 4, 1, txtFile);
+	socklen_t slen = sizeof(server);		//create the buffers, grab the first payload
+	char ACK[256];
+	char endOfArray[512] = "EOF";
+	
+	
+	while (!feof(txtFile)){
+		sendto(mysocket, chunksArray, 512, 0, (struct sockaddr *)&server, slen);
+		recvfrom(mysocket, ACK, 256, 0, (struct sockaddr *)&server, &slen);
+		cout << ACK << endl;		//loop to handle sending payloads and receiving acknowledgements from server
+		memset(chunksArray, '\0', sizeof(chunksArray));
+		results = fread(chunksArray, 4, 1, txtFile);
+	}
+	
+	
+	sendto(mysocket, chunksArray, 512, 0, (struct sockaddr *)&server, slen);
+	cout << chunksArray << std::flush;
+	sendto(mysocket, endOfArray, 512, 0, (struct sockaddr *)&server, slen);	//send the end of file signal
+	
+	
+	close(mysocket);
+	free(chunksArray);	//clean up of socket, buffer, and stream
+	fclose(txtFile);
+	return 0;
+};
+
+
+
--- Data Comm Program 2/server.cpp
+++ Data Comm Program 2/server.cpp
@@ -3,13 +3,60 @@
 //  Data Comm Program 2
 //
 //  Created by Hunter Holder and Chandler Dill on 3/10/17.
-//  Copyright © 2017 Hunter Holder. All rights reserved.
 //
 
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <assert.h>
+#include <string.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <time.h>
 #include <iostream>
+#include <fstream>
+#include <unistd.h>
+#include <stdlib.h>
+using namespace std;
+
 
 int main(int argc, const char * argv[]) {
-	// insert code here...
-	std::cout << "Hello, World!\n";
+	int mysocket = socket(AF_INET, SOCK_DGRAM, 0);	//setup UDP socket
+	int negotPort = atoi(argv[1]);
+	
+	struct sockaddr_in server;
+	memset((char *) &server, 0, sizeof(server));
+	
+	memset((char *) &server, 0, sizeof(server));
+	server.sin_family = AF_INET;
+	server.sin_port = htons(negotPort);		//UDP info and address handling
+	server.sin_addr.s_addr = htonl(INADDR_ANY);
+	
+	
+	bind(mysocket, (struct sockaddr *)&server, sizeof(server));
+	
+	
+	char textContents[512];
+	struct sockaddr_in client;		//setting up payload buffer and client variables
+	socklen_t clen = sizeof(client);
+	
+	
+	FILE * output;
+	output = fopen("output.txt", "w+");
+	
+	
+	while(strcmp(textContents,"EOF") != 0){		//loop through all packets recieved, then promptly uppercases them and send them back as acknowledgement
+		recvfrom(mysocket, textContents, 512, 0, (struct sockaddr *)&client, &clen);
+		if(strcmp(textContents, "EOF") != 0){
+			fputs(textContents, output);}
+		for(int count = 0; count < 4; count = count + 1){
+			textContents[count] = toupper(textContents[count]);
+		}
+		sendto(mysocket, textContents, 256, 0, (struct sockaddr *)&client, sizeof(client));
+	}
+	
+	fclose(output);
+	close(mysocket);
 	return 0;
 }
