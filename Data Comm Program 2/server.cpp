6e4663c79456af5ffe493112953558616f8e7af5
Chandler <ddillchandler@aol.com>
2017-03-23 21:59:36 -0500

    Add files via upload

--- Data Comm Program 2/server.cpp
+++ Data Comm Program 2/server.cpp
@@ -21,156 +21,124 @@
 #include <stdlib.h>
 using namespace std;
 
-#define ALLSEQNUMS 8
-
+#define SEQNUMAMOUNT 8
 // argv0: server
 // argv1: <emulatorName: hostname for the emulator>
 // argv2: <receiveFromEmulator: UDP port number used by the server to receive data from the emulator>
 // argv3: <sendToEumulator-Port: UDP port number used by the emulator to receive ACKs from the server>
 // argv4: <fileName: name of the file into which the received data is written>
 int main(int argc, const char ** argv) {
-	
-	// destination IP
-	struct hostent* destinationIP = gethostbyname(argv[1]);
-	if (destinationIP == NULL) {
-		fprintf(stderr, "- error: hostname does not exist");
+	////////////VARIABLE INITIALIZATIONvvvv
+	int nextSequenceNumber = 0;
+	bool transmissionKill = false;			//sequence variables
+	int receiverSequenceNumber;
+	int keepLooking;
+
+	char fileContents[32];
+	int typePacket;
+	char* dataInPacket;
+	packet incomingPacket(NULL, NULL, NULL, fileContents);	//das incoming packet buffer and info
+	char incomingPacketWindow[128];
+	char outgoingAcksWindow[128];
+
+	///////////CONNECTION INFORMATIONvvvv
+	struct hostent* clientIP = gethostbyname(argv[1]);		//command given for the IP of client
+	if (clientIP == NULL) {
+		perror("- error: hostname does not exist");
 		exit(EXIT_FAILURE);
 	}
-	
-	// set up ports
-	int receivingPort = atoi(argv[2]);
+	int receivingPort = atoi(argv[2]);	//port inits
 	int sendingPort = atoi(argv[3]);
-	
-	// sockets
-	int receivingSocket;
+	int receivingSocket;	//socket inits
 	int sendingSocket;
-	
-	// set up connection
+
+	//set up connection
 	struct sockaddr_in destination;
-	memset((char *)&destination, 0, sizeof(destination));
+	socklen_t sizeDestination = sizeof(destination);	//destination socket init
+	sizeDestination = sizeof destination;
+	memset((char *)&destination, 0, sizeDestination);
 	destination.sin_family = AF_INET;
-	bcopy((char *)destinationIP->h_addr, (char *)&destination.sin_addr.s_addr, destinationIP->h_length);
+	bcopy((char *)clientIP->h_addr, (char *)&destination.sin_addr.s_addr, clientIP->h_length);
 	destination.sin_port = htons(sendingPort);
-	socklen_t destination_len = sizeof(destination);
-	destination_len = sizeof destination;
-	
-	
-	struct sockaddr_in incoming;
-	memset((char *)&incoming, 0, sizeof(incoming));
-	socklen_t incoming_len;
-	incoming_len = sizeof incoming;
-	incoming.sin_family = AF_INET;
-	incoming.sin_port = htons(receivingPort);
-	incoming.sin_addr.s_addr = htonl(INADDR_ANY);
-	
-	//Create Sockets
-	int errorCheck;
-	
+
+	struct sockaddr_in receiver;
+	socklen_t sizeReceival = sizeof receiver;			//receiving socket init
+	memset((char *)&receiver, 0, sizeReceival);
+	receiver.sin_family = AF_INET;
+	receiver.sin_port = htons(receivingPort);
+	receiver.sin_addr.s_addr = htonl(INADDR_ANY);
+
 	sendingSocket = socket(AF_INET, SOCK_DGRAM, 0);
 	if (sendingSocket <= -1) {
-		perror("Could not open socket for sending file!\n");
+		perror("- error: couldn't open the socket\n");			//create the sending socket here
 		exit(EXIT_FAILURE);
 	}
+
+	int socketCheck;
 	receivingSocket = socket(AF_INET, SOCK_DGRAM, 0);
-	errorCheck = bind(receivingSocket, (sockaddr *)&incoming, incoming_len);
-	if (receivingSocket <= -1 || errorCheck <= -1){
-		perror("Could not open socket for receiving acknowledgements!");
+	socketCheck = bind(receivingSocket, (sockaddr *)&receiver, sizeReceival);	//create the receive socket here
+	if (receivingSocket <= -1){
+		perror("- error: could'nt open the ack socket");	//did socket get assigned?
 		exit(EXIT_FAILURE);
 	}
-	
-	//File Stuff
-	fstream arrival;
-	arrival.open("arrival.log", fstream::out);
-	fstream textfile;
-	textfile.open(argv[4], fstream::out);
-	
-	//Transmission Variables
-	int expected_seqnum = 0;
-	bool endTransmission = false;
-	int packet_type;
-	char* packet_data;
-	int incoming_seqnum;
-	int highest_inorder;
-	
-	//Packet Containers
-	char data[32];
-	packet incoming_packet(0, 0, 0, data);
-	char packet_buffer[128];
-	char ack_buffer[128];
-	
-	
-	while (true){
-		
-		memset(ack_buffer, 0, 128);
-		memset(packet_buffer, 0, 128);
-		memset(data, 0, 32);
-		
-		recvfrom(receivingSocket, packet_buffer, sizeof packet_buffer, 0, (struct sockaddr *)&incoming, &incoming_len);
-		incoming_packet.deserialize(packet_buffer);
-		
-		//Record the sequence number of the received packet in arrival.log
-		incoming_seqnum = incoming_packet.getSeqNum();
-		arrival << incoming_seqnum << "\n";
-		
-		//Check the type. Write to file if it's data. End transmission if it's EOT.
-		packet_type = incoming_packet.getType();
-		switch (packet_type){
-				
-			case 1: {
-				//Incoming Data Packet
-				if (incoming_seqnum == expected_seqnum){
-					
-					//ACK packet
-					packet ack_packet(0, incoming_seqnum, 0, 0);
-					ack_packet.serialize(ack_buffer);
-					sendto(sendingSocket, ack_buffer, sizeof ack_buffer, 0, (struct sockaddr*)&destination, destination_len);
-					
-					//write the incoming data to the file
-					packet_data = incoming_packet.getData();
-					for(int n=0; n<(incoming_packet.getLength()); n++){
-						textfile << packet_data[n];
-					}
-					
-					//increment variables
-					expected_seqnum = (expected_seqnum + 1) % ALLSEQNUMS;
-				}
-				
-				else{
-					
-					highest_inorder = (expected_seqnum + 7) % 8;
-					packet ack_packet(0, highest_inorder, 0, 0);
-					memset(packet_buffer, 0, 128);
-					ack_packet.serialize(packet_buffer);
-					sendto(sendingSocket, packet_buffer, sizeof packet_buffer, 0, (struct sockaddr*)&destination, destination_len);
-				}
-				
-				endTransmission = false;
-				break;
-			}
-				
-			case 3: {
-				//Client to Server EOT detected
-				packet eot_packet(2, expected_seqnum, 0, 0);
-				memset(packet_buffer, 0, 64);
-				eot_packet.serialize(packet_buffer);
-				sendto(sendingSocket, packet_buffer, sizeof packet_buffer, 0, (struct sockaddr*)&destination, destination_len);
-				endTransmission = true;
-				break;
-			}
-			default: {
-				//Type is either 0 (ACK) or 2 (server to client EOT). Neither make sense.
-				break;
-			}
-		}
-		
-		//Check if the EOT packet was received
-		if (endTransmission) break;
+	else if(socketCheck <= -1){
+		perror("- error: could'nt bind the ack socket");	//did socket bind correctly?
+		exit(EXIT_FAILURE);
 	}
-	
-	//Done. Close things up and return.
-	arrival.close();
-	textfile.close();
+
+//////////////QUICK FILE SETUPvvvv
+	fstream fileStream, incomingPackets;
+	fileStream.open(argv[4], fstream::out);	//open the file given in commands
+	incomingPackets.open("incomingPackets.log", fstream::out);	//launch log for packets arriving
+
+////////////"Oh man. What even-?!" GBN CALCULATION PARTvvvv
+	while (true){
+		memset(fileContents, 0, 32);
+		memset(outgoingAcksWindow, 0, 128);		//init the current ack/incoming windows again
+		memset(incomingPacketWindow, 0, 128);
+
+		recvfrom(receivingSocket, incomingPacketWindow, sizeof incomingPacketWindow, 0, (struct sockaddr *)&receiver, &sizeReceival);
+		incomingPacket.deserialize(incomingPacketWindow);		//unpack the receivedpacket and write to incomingPackets.log
+		receiverSequenceNumber = incomingPacket.getSeqNum();
+		incomingPackets << receiverSequenceNumber << "\n";
+
+		typePacket = incomingPacket.getType();		//if the packet type is file contents, write to file. Else, it's EOT and must be ended
+		if(typePacket == 1){
+				if (receiverSequenceNumber == nextSequenceNumber){ 		//is this packet the one we need? Sweet, dude. Use it.
+					packet correctAcknowledgementPacket(0, receiverSequenceNumber, 0, NULL);
+					correctAcknowledgementPacket.serialize(outgoingAcksWindow);		//we liked that packet, pack up the ack and send it
+					sendto(sendingSocket, outgoingAcksWindow, sizeof outgoingAcksWindow, 0, (struct sockaddr*)&destination, sizeDestination);
+
+					dataInPacket = incomingPacket.getData();
+					for(int n=0; n<(incomingPacket.getLength()); n++){	//write the data from the packet to file after ack is sent
+						fileStream << dataInPacket[n];
+					}
+					nextSequenceNumber = (nextSequenceNumber + 1) % SEQNUMAMOUNT; //next correct sequence number please
+				}
+				else {
+					/*FUUUUUUUUUUUUUUUUCK*/
+					keepLooking = (nextSequenceNumber + 7) % 8;
+					packet incorrectAcknowledgementPacket(0, keepLooking, 0, NULL);
+					memset(outgoingAcksWindow, 0, 128);
+					incorrectAcknowledgementPacket.serialize(outgoingAcksWindow);
+					sendto(sendingSocket, outgoingAcksWindow, sizeof outgoingAcksWindow, 0, (struct sockaddr*)&destination, sizeDestination);
+				}
+				/*FUUUUUUUUUUUUUUUUUCK*/
+			}
+			else if(typePacket == 3){
+				packet killSignal(2, nextSequenceNumber, 0, NULL);		//sending the transmissionKill packet for EOT because, basically anything other than fileContents
+				memset(outgoingAcksWindow, 0, 64);
+				killSignal.serialize(outgoingAcksWindow);
+				sendto(sendingSocket, outgoingAcksWindow, sizeof outgoingAcksWindow, 0, (struct sockaddr*)&destination, sizeDestination);
+				transmissionKill = true;		//set the flag to end the while loop
+			}
+			if (transmissionKill == true)
+				break;	//self-explanatory, was EOT flag set? break.
+		}
+
 	close(receivingSocket);
 	close(sendingSocket);
+	incomingPackets.close();		//finishing sockets and fstreams
+	fileStream.close();
 	return 0;
 }
